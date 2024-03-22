c60160b37a40fc3c81a19eed5fdbe78577310c6f
Tuanpnse12032001 <122070367+Tuanpnse12032001@users.noreply.github.com>
2024-03-22 16:59:45 +0700

    fix the api for the Product

--- src/main/java/com/example/securityl/controllers/PaymentController.java
+++ src/main/java/com/example/securityl/controllers/PaymentController.java
@@ -1,26 +1,26 @@
-//package com.example.securityl.controllers;
-//
-//
-//import com.example.securityl.models.PaymentStatus;
-//import com.example.securityl.services.IPaymentService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@CrossOrigin
-//@RequestMapping("${api.prefix}/payment-status")
-//@RequiredArgsConstructor
-//public class PaymentController {
-//    private final IPaymentService paymentService;
-//    @GetMapping("")
-//    public ResponseEntity<List<PaymentStatus>> getAll(){
-//        List<PaymentStatus> paymentStatuses = paymentService.getAll();
-//        return ResponseEntity.ok(paymentStatuses);
-//    }
-//}
+package com.example.securityl.controllers;
+
+
+import com.example.securityl.models.PaymentStatus;
+import com.example.securityl.services.IPaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("${api.prefix}/payment-status")
+@RequiredArgsConstructor
+public class PaymentController {
+    private final IPaymentService paymentService;
+    @GetMapping("")
+    public ResponseEntity<List<PaymentStatus>> getAll(){
+        List<PaymentStatus> paymentStatuses = paymentService.getAll();
+        return ResponseEntity.ok(paymentStatuses);
+    }
+}
