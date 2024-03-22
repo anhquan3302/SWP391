c146eff37692242ef51540697b72f155b23f2f22
Tuanpnse12032001 <122070367+Tuanpnse12032001@users.noreply.github.com>
2024-03-22 16:58:40 +0700

    hot fix for api's Blog

--- src/main/java/com/example/securityl/controllers/BlogController.java
+++ src/main/java/com/example/securityl/controllers/BlogController.java
@@ -1,137 +1,137 @@
-//    package com.example.securityl.controllers;
-//
-//
-//    import com.example.securityl.Responses.BlogListResponse;
-//    import com.example.securityl.Responses.BlogResponse;
-//    import com.example.securityl.converter.BlogConverter;
-//    import com.example.securityl.dtos.BlogDto;
-//    import com.example.securityl.models.Blog;
-//    import com.example.securityl.models.User;
-//    import com.example.securityl.repositories.UserRepository;
-//    import com.example.securityl.services.impl.BlogService;
-//    import jakarta.persistence.EntityNotFoundException;
-//    import jakarta.validation.Valid;
-//    import lombok.RequiredArgsConstructor;
-//
-//    import org.springframework.data.domain.Page;
-//    import org.springframework.data.domain.PageRequest;
-//    import org.springframework.http.HttpStatus;
-//    import org.springframework.http.ResponseEntity;
-//    import org.springframework.web.bind.annotation.*;
-//    import org.springframework.web.multipart.MultipartFile;
-//
-//    import java.io.IOException;
-//    import java.util.List;
-//
-//
-//    @RestController
-//    @RequestMapping("api/v1/blogs")
-//    @RequiredArgsConstructor
-//    @CrossOrigin
-//    public class BlogController {
-//        private final BlogService blogService;
-//        private final UserRepository userRepository;
-//
-//        @CrossOrigin
-//        @GetMapping("/get-blog-detail/{id}")
-//        public ResponseEntity<?> getBlogById(@PathVariable("id") Long id) {
-//            try {
-//                Blog blog = blogService.getBlogById(id);
-//                BlogResponse blogResponse = BlogConverter.toResponse(blog); // Chuyển đổi từ Blog sang BlogResponse
-//                return new ResponseEntity<>(blogResponse, HttpStatus.OK);
-//            } catch (Exception e) {
-//                return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-//            }
-//        }
-//
-//        @CrossOrigin
-//        @GetMapping("/get_all_blogs")
-//        public ResponseEntity<BlogListResponse> getAllBlogs(
-//                @RequestParam(value = "keyword", required = false) String keyword,
-//                @RequestParam(value = "page", defaultValue = "0") int page,
-//                @RequestParam(value = "size", defaultValue = "10") int size) {
-//            PageRequest pageRequest = PageRequest.of(page, size);
-//            Page<BlogResponse> blogsPage = blogService.getAllBlogs(keyword, pageRequest, null);
-//
-//
-//            List<BlogResponse> blogList = blogsPage.getContent();
-//
-//            for (BlogResponse blog : blogList) {
-//                Long userBlogId = blog.getUserBlogId();
-//                User user = userRepository.findById(userBlogId).orElse(null);
-//                if (user != null) {
-//                    blog.setUserFullName(user.getFullName());
-//                }
-//            }
-//
-//            int totalPages = blogsPage.getTotalPages();
-//            return ResponseEntity.ok(BlogListResponse.builder()
-//                    .blogs(blogList)
-//                    .totalPages(totalPages)
-//                    .build());
-//        }
-//
-//        @CrossOrigin
-//        @PostMapping("/create_blog")
-//        public ResponseEntity<?> createBlog(@RequestBody @Valid BlogDto blogDto) {
-//            try {
-//                Blog createdBlog = blogService.createBlog(blogDto);
-//                return ResponseEntity.status(HttpStatus.CREATED).body(createdBlog);
-//            } catch (EntityNotFoundException e) {
-//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-//            }
-//        }
-//
-//
-//        @PutMapping("/update_blog/{blogId}")
-//        public ResponseEntity<?> updateBlog(
-//                @PathVariable Long blogId,
-//                @RequestBody @Valid BlogDto updatedBlogDto) {
-//            try {
-//                Blog updatedBlog = blogService.updateBlog(blogId, updatedBlogDto);
-//                BlogResponse blogResponse = BlogConverter.toResponse(updatedBlog);
-//                return ResponseEntity.ok(blogResponse);
-//            } catch (RuntimeException e) {
-//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Blog is deactivated.");
-//            }
-//        }
-//
-//
-//
-//        @PostMapping("/{blogId}/upload_thumbnail")
-//        public ResponseEntity<String> uploadImageToCloudinary(@PathVariable Long blogId, @RequestParam("thumbnail") MultipartFile thumbnail) {
-//            try {
-//                String thumbnailUrl = blogService.uploadThumbnailToCloudinary(blogId, thumbnail);
-//                return ResponseEntity.ok("Upload_Thumbnail successful!!" + thumbnailUrl);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload image");
-//            }
-//        }
-//
-//
-//
-//
-//        @DeleteMapping("/delete_blog/{blogId}")
-//        public ResponseEntity<?> deleteBlog(@PathVariable Long blogId) {
-//            try {
-//                blogService.deleteBlog(blogId);
-//                return ResponseEntity.ok().build();
-//            } catch (EntityNotFoundException e) {
-//                return ResponseEntity.notFound().build();
-//            }
-//        }
-//
-//
-//        @CrossOrigin
-//        @GetMapping("/latest-three-blogs")
-//        public ResponseEntity<List<BlogResponse>> getLatestThreeBlogs() {
-//            try {
-//                List<BlogResponse> latestBlogs = blogService.getLatestThreeBlogs();
-//                return new ResponseEntity<>(latestBlogs, HttpStatus.OK);
-//            } catch (Exception e) {
-//                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//            }
-//        }
-//
-//    }
+    package com.example.securityl.controllers;
+
+
+    import com.example.securityl.Responses.BlogListResponse;
+    import com.example.securityl.Responses.BlogResponse;
+    import com.example.securityl.converter.BlogConverter;
+    import com.example.securityl.dtos.BlogDto;
+    import com.example.securityl.models.Blog;
+    import com.example.securityl.models.User;
+    import com.example.securityl.repositories.UserRepository;
+    import com.example.securityl.services.impl.BlogService;
+    import jakarta.persistence.EntityNotFoundException;
+    import jakarta.validation.Valid;
+    import lombok.RequiredArgsConstructor;
+
+    import org.springframework.data.domain.Page;
+    import org.springframework.data.domain.PageRequest;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
+    import org.springframework.web.multipart.MultipartFile;
+
+    import java.io.IOException;
+    import java.util.List;
+
+
+    @RestController
+    @RequestMapping("api/v1/blogs")
+    @RequiredArgsConstructor
+    @CrossOrigin
+    public class BlogController {
+        private final BlogService blogService;
+        private final UserRepository userRepository;
+
+        @CrossOrigin
+        @GetMapping("/get-blog-detail/{id}")
+        public ResponseEntity<?> getBlogById(@PathVariable("id") Long id) {
+            try {
+                Blog blog = blogService.getBlogById(id);
+                BlogResponse blogResponse = BlogConverter.toResponse(blog); // Chuyển đổi từ Blog sang BlogResponse
+                return new ResponseEntity<>(blogResponse, HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+            }
+        }
+
+        @CrossOrigin
+        @GetMapping("/get_all_blogs")
+        public ResponseEntity<BlogListResponse> getAllBlogs(
+                @RequestParam(value = "keyword", required = false) String keyword,
+                @RequestParam(value = "page", defaultValue = "0") int page,
+                @RequestParam(value = "size", defaultValue = "10") int size) {
+            PageRequest pageRequest = PageRequest.of(page, size);
+            Page<BlogResponse> blogsPage = blogService.getAllBlogs(keyword, pageRequest, null);
+
+
+            List<BlogResponse> blogList = blogsPage.getContent();
+
+            for (BlogResponse blog : blogList) {
+                Long userBlogId = blog.getUserBlogId();
+                User user = userRepository.findById(userBlogId).orElse(null);
+                if (user != null) {
+                    blog.setUserFullName(user.getFullName());
+                }
+            }
+
+            int totalPages = blogsPage.getTotalPages();
+            return ResponseEntity.ok(BlogListResponse.builder()
+                    .blogs(blogList)
+                    .totalPages(totalPages)
+                    .build());
+        }
+
+        @CrossOrigin
+        @PostMapping("/create_blog")
+        public ResponseEntity<?> createBlog(@RequestBody @Valid BlogDto blogDto) {
+            try {
+                Blog createdBlog = blogService.createBlog(blogDto);
+                return ResponseEntity.status(HttpStatus.CREATED).body(createdBlog);
+            } catch (EntityNotFoundException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            }
+        }
+
+
+        @PutMapping("/update_blog/{blogId}")
+        public ResponseEntity<?> updateBlog(
+                @PathVariable Long blogId,
+                @RequestBody @Valid BlogDto updatedBlogDto) {
+            try {
+                Blog updatedBlog = blogService.updateBlog(blogId, updatedBlogDto);
+                BlogResponse blogResponse = BlogConverter.toResponse(updatedBlog);
+                return ResponseEntity.ok(blogResponse);
+            } catch (RuntimeException e) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Blog is deactivated.");
+            }
+        }
+
+
+
+        @PostMapping("/{blogId}/upload_thumbnail")
+        public ResponseEntity<String> uploadImageToCloudinary(@PathVariable Long blogId, @RequestParam("thumbnail") MultipartFile thumbnail) {
+            try {
+                String thumbnailUrl = blogService.uploadThumbnailToCloudinary(blogId, thumbnail);
+                return ResponseEntity.ok("Upload_Thumbnail successful!!" + thumbnailUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload image");
+            }
+        }
+
+
+
+
+        @DeleteMapping("/delete_blog/{blogId}")
+        public ResponseEntity<?> deleteBlog(@PathVariable Long blogId) {
+            try {
+                blogService.deleteBlog(blogId);
+                return ResponseEntity.ok().build();
+            } catch (EntityNotFoundException e) {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+
+        @CrossOrigin
+        @GetMapping("/latest-three-blogs")
+        public ResponseEntity<List<BlogResponse>> getLatestThreeBlogs() {
+            try {
+                List<BlogResponse> latestBlogs = blogService.getLatestThreeBlogs();
+                return new ResponseEntity<>(latestBlogs, HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+    }
--- src/main/java/com/example/securityl/controllers/OrderDetailController.java
+++ src/main/java/com/example/securityl/controllers/OrderDetailController.java
@@ -1,101 +1,101 @@
-//package com.example.securityl.controllers;
-//
-//
-//import com.example.securityl.Responses.OrderDetailResponse;
-//import com.example.securityl.components.LocalizationUtils;
-//import com.example.securityl.converter.OrderDetailConverter;
-//import com.example.securityl.dtos.OrderDetailDto;
-//import com.example.securityl.dtos.chartDto.SalesChartDTO;
-//import com.example.securityl.dtos.chartDto.TopSellingProductDTO;
-//import com.example.securityl.exceptions.DataNotFoundException;
-//import com.example.securityl.models.OrderDetail;
-//import com.example.securityl.services.IOrderDetailService;
-//import com.example.securityl.utills.MessageKeys;
-//import jakarta.validation.Valid;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.validation.FieldError;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("${api.prefix}/orders-detail")
-//@RequiredArgsConstructor
-//@CrossOrigin
-//public class OrderDetailController {
-//    private final IOrderDetailService orderDetailService;
-//    private final LocalizationUtils localizationUtils;
-//
-//    @PostMapping("")
-//    public ResponseEntity<?> createOrderDetail(
-//            @Valid @RequestBody OrderDetailDto orderDetailDto,
-//            BindingResult result
-//    ) {
-//        try {
-//            if (result.hasErrors()) {
-//                List<String> errorMessages = result.getFieldErrors()
-//                        .stream()
-//                        .map(FieldError::getDefaultMessage)
-//                        .toList();
-//                return ResponseEntity.badRequest().body(errorMessages);
-//            }
-//            OrderDetail orderDetailResponse = orderDetailService.createOrderDetail(orderDetailDto);
-//            return ResponseEntity.ok(orderDetailResponse);
-//        } catch (Exception e) {
-//            return ResponseEntity.badRequest().body(e.getMessage());
-//        }
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<?> getOrderDetail(
-//            @Valid @PathVariable("id") Long id) throws DataNotFoundException {
-//        OrderDetail orderDetail = orderDetailService.getOrderDetail(id);
-//        return ResponseEntity.ok().body(OrderDetailConverter.fromOrderDetail(orderDetail));
-//    }
-//
-//    @GetMapping("/order/{orderId}")
-//    public ResponseEntity<?> getOrderDetails(
-//            @Valid @PathVariable("orderId") Long orderId
-//    ) {
-//        List<OrderDetail> orderDetails = orderDetailService.findByOrderId(orderId);
-//        List<OrderDetailResponse> orderDetailResponses = orderDetails
-//                .stream()
-//                .map(OrderDetailConverter::fromOrderDetail)
-//                .toList();
-//        return ResponseEntity.ok(orderDetailResponses);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<?> updateOrderDetail(
-//            @Valid @PathVariable("id") Long id,
-//            @RequestBody OrderDetailDto orderDetailDTO) {
-//        try {
-//            OrderDetail orderDetail = orderDetailService.updateOrderDetail(id, orderDetailDTO);
-//            return ResponseEntity.ok().body(orderDetail);
-//        } catch (DataNotFoundException e) {
-//            return ResponseEntity.badRequest().body(e.getMessage());
-//        }
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<?> deleteOrderDetail(
-//            @Valid @PathVariable("id") Long id) {
-//        orderDetailService.deleteById(id);
-//        return ResponseEntity.ok()
-//                .body(localizationUtils
-//                        .getLocalizedMessage(MessageKeys.DELETE_ORDER_DETAIL_SUCCESSFULLY));
-//    }
-//
-//    @GetMapping("/chart")
-//    public ResponseEntity<List<SalesChartDTO>> getTotalAmountSoldByDate() {
-//        List<SalesChartDTO> totalAmounts = orderDetailService.getTotalAmountSoldByDate();
-//        return ResponseEntity.ok(totalAmounts);
-//    }
-//
-//    @GetMapping("/chart-top-sale-product")
-//    public List<TopSellingProductDTO> getMostSoldProductsByDate() {
-//        return orderDetailService.findMostSoldProductsByDate();
-//    }
-//}
+package com.example.securityl.controllers;
+
+
+import com.example.securityl.Responses.OrderDetailResponse;
+import com.example.securityl.components.LocalizationUtils;
+import com.example.securityl.converter.OrderDetailConverter;
+import com.example.securityl.dtos.OrderDetailDto;
+import com.example.securityl.dtos.chartDto.SalesChartDTO;
+import com.example.securityl.dtos.chartDto.TopSellingProductDTO;
+import com.example.securityl.exceptions.DataNotFoundException;
+import com.example.securityl.models.OrderDetail;
+import com.example.securityl.services.IOrderDetailService;
+import com.example.securityl.utills.MessageKeys;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/orders-detail")
+@RequiredArgsConstructor
+@CrossOrigin
+public class OrderDetailController {
+    private final IOrderDetailService orderDetailService;
+    private final LocalizationUtils localizationUtils;
+
+    @PostMapping("")
+    public ResponseEntity<?> createOrderDetail(
+            @Valid @RequestBody OrderDetailDto orderDetailDto,
+            BindingResult result
+    ) {
+        try {
+            if (result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            OrderDetail orderDetailResponse = orderDetailService.createOrderDetail(orderDetailDto);
+            return ResponseEntity.ok(orderDetailResponse);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOrderDetail(
+            @Valid @PathVariable("id") Long id) throws DataNotFoundException {
+        OrderDetail orderDetail = orderDetailService.getOrderDetail(id);
+        return ResponseEntity.ok().body(OrderDetailConverter.fromOrderDetail(orderDetail));
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<?> getOrderDetails(
+            @Valid @PathVariable("orderId") Long orderId
+    ) {
+        List<OrderDetail> orderDetails = orderDetailService.findByOrderId(orderId);
+        List<OrderDetailResponse> orderDetailResponses = orderDetails
+                .stream()
+                .map(OrderDetailConverter::fromOrderDetail)
+                .toList();
+        return ResponseEntity.ok(orderDetailResponses);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateOrderDetail(
+            @Valid @PathVariable("id") Long id,
+            @RequestBody OrderDetailDto orderDetailDTO) {
+        try {
+            OrderDetail orderDetail = orderDetailService.updateOrderDetail(id, orderDetailDTO);
+            return ResponseEntity.ok().body(orderDetail);
+        } catch (DataNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteOrderDetail(
+            @Valid @PathVariable("id") Long id) {
+        orderDetailService.deleteById(id);
+        return ResponseEntity.ok()
+                .body(localizationUtils
+                        .getLocalizedMessage(MessageKeys.DELETE_ORDER_DETAIL_SUCCESSFULLY));
+    }
+
+    @GetMapping("/chart")
+    public ResponseEntity<List<SalesChartDTO>> getTotalAmountSoldByDate() {
+        List<SalesChartDTO> totalAmounts = orderDetailService.getTotalAmountSoldByDate();
+        return ResponseEntity.ok(totalAmounts);
+    }
+
+    @GetMapping("/chart-top-sale-product")
+    public List<TopSellingProductDTO> getMostSoldProductsByDate() {
+        return orderDetailService.findMostSoldProductsByDate();
+    }
+}
