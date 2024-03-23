a9dd5efb46a10778b7a9d6332f6b0b14b202340b
Tuanpnse12032001 <122070367+Tuanpnse12032001@users.noreply.github.com>
2024-03-23 12:33:26 +0700

    hot fix for service logic

--- src/main/java/com/example/securityl/services/IBookingService.java
+++ src/main/java/com/example/securityl/services/IBookingService.java
@@ -1,22 +1,22 @@
-//package com.example.securityl.services;
-//
-//
-//import com.example.securityl.dtos.BookingDto;
-//import com.example.securityl.exceptions.DataNotFoundException;
-//import com.example.securityl.models.Booking;
-//import com.example.securityl.models.Design;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//
-//public interface IBookingService {
-//
-//    Design createDesign(Design design);
-//
-//    Booking getBookingById(Long bookingId);
-//
-//    BookingDto registerBooking(BookingDto bookingDto) throws DataNotFoundException;
-//
-//    Page<BookingDto> getAllBookingDtos(Pageable pageable);
-//
-//    Page<BookingDto> getAllBookingDtosByUserId(Pageable pageable, Long userId);
-//}
+package com.example.securityl.services;
+
+
+import com.example.securityl.dtos.BookingDto;
+import com.example.securityl.exceptions.DataNotFoundException;
+import com.example.securityl.models.Booking;
+import com.example.securityl.models.Design;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IBookingService {
+
+    Design createDesign(Design design);
+
+    Booking getBookingById(Long bookingId);
+
+    BookingDto registerBooking(BookingDto bookingDto) throws DataNotFoundException;
+
+    Page<BookingDto> getAllBookingDtos(Pageable pageable);
+
+    Page<BookingDto> getAllBookingDtosByUserId(Pageable pageable, Long userId);
+}
