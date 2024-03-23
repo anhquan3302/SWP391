53b7e9fe088119195eec431fcec02f70d9699f84
Tuanpnse12032001 <122070367+Tuanpnse12032001@users.noreply.github.com>
2024-03-23 12:31:44 +0700

    fix blog service

--- src/main/java/com/example/securityl/services/impl/CategoriesBlogService.java
+++ src/main/java/com/example/securityl/services/impl/CategoriesBlogService.java
@@ -1,38 +1,38 @@
-package com.example.securityl.services.impl;
-
-
-import com.example.securityl.models.CategoryBlog;
-import com.example.securityl.repositories.CategoryBlogRepository;
-import com.example.securityl.services.ICategoriesBlogService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-public class CategoriesBlogService implements ICategoriesBlogService {
-   private final CategoryBlogRepository categoryBlogRepository;
-
-   @CrossOrigin
-    @Override
-    public List<CategoryBlog> getAllCategoriesBlog() {
-        return categoryBlogRepository.findAll();
-    }
-
-    @Override
-    public CategoryBlog getCategoryById(Long id) {
-        return categoryBlogRepository.findById(id).orElse(null);
-    }
-
-    @Override
-    public CategoryBlog saveCategory(CategoryBlog categoryBlog) {
-        return categoryBlogRepository.save(categoryBlog);
-    }
-
-    @Override
-    public void deleteCategory(Long id) {
-        categoryBlogRepository.deleteById(id);
-    }
-}
+//package com.example.securityl.services.impl;
+//
+//
+//import com.example.securityl.models.CategoryBlog;
+//import com.example.securityl.repositories.CategoryBlogRepository;
+//import com.example.securityl.services.ICategoriesBlogService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class CategoriesBlogService implements ICategoriesBlogService {
+//   private final CategoryBlogRepository categoryBlogRepository;
+//
+//   @CrossOrigin
+//    @Override
+//    public List<CategoryBlog> getAllCategoriesBlog() {
+//        return categoryBlogRepository.findAll();
+//    }
+//
+//    @Override
+//    public CategoryBlog getCategoryById(Long id) {
+//        return categoryBlogRepository.findById(id).orElse(null);
+//    }
+//
+//    @Override
+//    public CategoryBlog saveCategory(CategoryBlog categoryBlog) {
+//        return categoryBlogRepository.save(categoryBlog);
+//    }
+//
+//    @Override
+//    public void deleteCategory(Long id) {
+//        categoryBlogRepository.deleteById(id);
+//    }
+//}
